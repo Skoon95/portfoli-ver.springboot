481f0c04765b76f2f0645365fda27c520d058a2d
YSH <tjrgns21@naver.com>
2023-05-23 12:23:34 +0900

    portfolio ver springboot

--- src/main/resources/static/resources/scripts/portfolio.js
+++ src/main/resources/static/resources/scripts/portfolio.js
@@ -1,8 +1,30 @@
-
-// 메뉴 상태변경 버튼을 누르면 .container 상태를 변경하도록 처리
-window.addEventListener("load",function () {
-    document.querySelector(".toggle").addEventListener("click",function (){
+window.addEventListener("load", function() {
+    document.querySelector(".toggle").addEventListener("click", function() {
         document.querySelector(".container").classList.toggle("collapse");
     });
 });
 
+// window.addEventListener('scroll', function() {
+//     var sections = document.querySelectorAll('.contents > section');
+//     var projectSection = document.getElementById('project');
+//
+//     sections.forEach(function(section) {
+//         if (isElementInViewport(section)) {
+//             section.classList.add('show');
+//         }
+//     });
+//
+//     if (isElementInViewport(projectSection)) {
+//         projectSection.classList.add('show');
+//     }
+// });
+//
+// function isElementInViewport(element) {
+//     var rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
